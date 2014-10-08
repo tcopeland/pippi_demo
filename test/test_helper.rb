89c115cb19085b0623e5b0721c7193991d33a3ce
Tom Copeland <tom.copeland@livingsocial.com>
2014-10-08 18:46:51 -0400

    Fixed autorunner

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,15 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
+
+# This must be before test/unit is required
+if ENV['USE_PIPPI'].present?
+  Pippi::AutoRunner.new
+  class ActiveSupport::Cache::Store
+    def write(*_)
+    end
+  end
+end
+
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
@@ -12,8 +22,4 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
-end
-
-if ENV['USE_PIPPI'].present?
-  Pippi::AutoRunner.new
 end
